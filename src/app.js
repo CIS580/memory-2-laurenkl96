24e7ac6b4ba6a1699af82d058e915c8e3d0cff86
laurenkl96 <lklynch@ksu.edu>
2016-09-28 15:20:42 -0500

    class update

--- src/app.js
+++ src/app.js
@@ -14,15 +14,44 @@ var cards = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
 var board = [];
 while(cards.length > 0) {
   var index = Math.floor(Math.random() * (cards.length - 1));
-  board.push({card: cards[index], flip: true});
+  board.push({card: cards[index], flip: false});
   cards.splice(index, 1);
 }
-console.log(board);
+var state = "waiting 1";
+var points = [0,0];
+var player = 0;
+var c1;
 
 // TODO: Place the cards on the board in random order
 
 canvas.onclick = function(event) {
   event.preventDefault();
+  var x = Math.floor((event.clientX-3)/165);
+  var y = Math.floor((event.clientY-3)/165);
+  var c = board[y*6+x];
+  if(!c || c.flip) return;
+  c.flip = true;
+  switch(state){
+    case "waiting 1":
+      c1 = c;
+      state = "waiting 2";
+      break;
+    case "waiting 2":
+    c2 = c;
+    state = "waiting for timer";
+    setTimeout(function(){
+      if(c1.card == c.card){
+        points[player] ++;
+      }else{
+        c1.flip = false;
+        c.flip = false;
+        player = +!player;//this is not a typo
+      }
+    }, 3000);
+    
+      break;
+
+  }
   // TODO: determine which card was clicked on
   // TODO: determine what to do
 }
@@ -76,7 +105,7 @@ function render(elapsedTime, ctx) {
         );
       } else {
         // draw the back of the card (212x212px)
-        ctx.fillStyle = "#3333ff";
+        ctx.fillStyle = "	#9932CC";
         ctx.fillRect(x * 165 + 3, y * 165 + 3, 160, 160);
       }
     }
